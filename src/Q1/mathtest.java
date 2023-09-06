887a0c926e1fe21fa05ad44c76fe4b491dfdc8a5
madonna.k <krazykayden77@gmail.com>
2023-09-06 11:36:32 -0500

    update 9/5

---
+++ src/Q1/mathtest.java
@@ -0,0 +1,22 @@
+package Q1;
+
+public class mathtest {
+    public static void main(String[] args) {
+        int twocubed = (int)Math.pow(2, 3);
+
+        int num1 = 5;
+        int num2 = 5;
+        int num3 = 5;
+        int num4 = 5;
+        int sum = num1 + num2 + num3 + num4;
+        sum /= 4;
+        int test = 3/4;
+        System.out.println(test);
+
+        double mywallet = 2395.543634636;
+        double mywrounded = Math.round(mywallet * 100.0) / 100.0;
+
+        System.out.println(mywrounded);
+        System.out.printf("mywrounded = %.2f\n", mywallet);
+    }
+}
--- src/Q1/prog54b.java
+++ src/Q1/prog54b.java
@@ -1,7 +1,33 @@
 package Q1;
-
+import java.util.*;
 public class prog54b {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter 1st number: ");
+        int num1 = input.nextInt();
+        System.out.println();
+
+        System.out.print("Please enter 2nd number: ");
+        int num2 = input.nextInt();
+        System.out.println();
+
+        System.out.print("Please enter 3rd number: ");
+        int num3 = input.nextInt();
+        System.out.println();
+
+        System.out.print("Please enter 4th number: ");
+        int num4 = input.nextInt();
+        System.out.println();
+
+        int sum = num1 + num2 + num3 + num4;
+        double average = sum / 4;
+        double myaverounded = Math.round(average * 100.0) / 100.0;
+
+
+        System.out.println(myaverounded);
+        System.out.println("Sum = " + sum);
+        System.out.printf("Average = %.2f\n",  average);
 
     }
 }
