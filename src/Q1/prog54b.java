1ab8a770fd5af60ae991e205eb4e8817258952c3
madonna.k <krazykayden77@gmail.com>
2023-09-07 10:59:23 -0500

    update 9/5

--- src/Q1/prog54b.java
+++ src/Q1/prog54b.java
@@ -21,13 +21,23 @@ public class prog54b {
         System.out.println();
 
         int sum = num1 + num2 + num3 + num4;
-        double average = sum / 4;
-        double myaverounded = Math.round(average * 100.0) / 100.0;
+        double average = sum / 4.0;
 
 
-        System.out.println(myaverounded);
+
         System.out.println("Sum = " + sum);
-        System.out.printf("Average = %.2f\n",  average);
+        System.out.println("Average = " + average);
+
 
     }
 }
+/*
+Please enter 2nd number: 821
+
+Please enter 3rd number: 369
+
+Please enter 4th number: 562
+
+Sum = 2227
+Average = 556.75
+ */
