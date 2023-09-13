a697e8d9c7c6f6a40b45f95b76d144d538a98752
madonna.k <krazykayden77@gmail.com>
2023-09-13 11:35:55 -0500

    update 9/8

--- src/Q1/prog58b.java
+++ src/Q1/prog58b.java
@@ -1,7 +1,46 @@
 package Q1;
+import java.util.*;
+import java.util.Scanner;
 
 public class prog58b {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter Amount: ");
+        double Amount = input.nextDouble();
+        System.out.println();
+        // This is P
+        System.out.print("Please enter rate of interest: ");
+        double interest = input.nextDouble();
+        System.out.println();
+        // This is R
+        System.out.print("Please enter number of days compounded per year: ");
+        double per_year = input.nextDouble();
+        System.out.println();
+        // This is N
+        System.out.print("Please enter the amount of days that the money has been in the savings account: ");
+        double total_days = input.nextDouble();
+        System.out.println();
+        // This is t
+        double interest_conversion = interest / 100;
+
+        double interest_a = Amount * interest_conversion;
+        double interest_earned = Amount - interest_a;
+        double Total_amount = Amount * ((1 + ((.01 * interest_earned)/ per_year)Math.pow(per_year * total_days / 365)));
+
+        System.out.println("Money originally put in savings = " + Amount);
+        System.out.println("Rate of interest = " + interest);
+        System.out.println("number of days compounded per year = " + per_year);
+        System.out.println("Number of days money has been in savings account = " + total_days);
+        System.out.println();
+        System.out.println("the amount of interest earned is ", interest_earned);
+        System.out.println("The amount currently in the account is ", Total_amount);
+
+
+
 
     }
 }
+/*
+A = P * ((1 + ((.01 * r)/ n) n * t/ 365))
+ */
---
+++ src/Q1/prog58h.java
@@ -0,0 +1,7 @@
+package Q1;
+
+public class prog58h {
+    public static void main(String[] args) {
+
+    }
+}
