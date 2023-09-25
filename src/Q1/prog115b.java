7a5f04880bbe0b24454c66c54d9b44c74404f65f
kayden <krazykayden77@gmail.com>
2023-09-25 16:49:13 -0500

    9/25

---
+++ src/Q1/prog115b.java
@@ -0,0 +1,15 @@
+package Q1;
+import java.util.*;
+public class prog115b {
+    public static void main(String[] args) {
+        int num = 0;
+
+        while (num == 0) {
+            num += 2;
+            if (num == 32) {
+                System.out.println(num);
+            }
+
+        }
+    }
+}
---
+++ src/Q1/prog122a.java
@@ -0,0 +1,314 @@
+package Q1;
+import java.util.*;
+public class prog122a {
+    public static void main(String[] args) {
+        for (int lcv = 0; lcv < 50; lcv++) {
+            int num = lcv+1;
+            double sq = num * num;
+            double sqrt = Math.sqrt(num);
+            System.out.println("\nNumber: " + num + "\n\nSquared: " + sq + "\n\nSquare Root: " + sqrt);
+        }
+    }
+}
+/*
+Number: 1
+
+Squared: 1.0
+
+Square Root: 1.0
+
+Number: 2
+
+Squared: 4.0
+
+Square Root: 1.4142135623730951
+
+Number: 3
+
+Squared: 9.0
+
+Square Root: 1.7320508075688772
+
+Number: 4
+
+Squared: 16.0
+
+Square Root: 2.0
+
+Number: 5
+
+Squared: 25.0
+
+Square Root: 2.23606797749979
+
+Number: 6
+
+Squared: 36.0
+
+Square Root: 2.449489742783178
+
+Number: 7
+
+Squared: 49.0
+
+Square Root: 2.6457513110645907
+
+Number: 8
+
+Squared: 64.0
+
+Square Root: 2.8284271247461903
+
+Number: 9
+
+Squared: 81.0
+
+Square Root: 3.0
+
+Number: 10
+
+Squared: 100.0
+
+Square Root: 3.1622776601683795
+
+Number: 11
+
+Squared: 121.0
+
+Square Root: 3.3166247903554
+
+Number: 12
+
+Squared: 144.0
+
+Square Root: 3.4641016151377544
+
+Number: 13
+
+Squared: 169.0
+
+Square Root: 3.605551275463989
+
+Number: 14
+
+Squared: 196.0
+
+Square Root: 3.7416573867739413
+
+Number: 15
+
+Squared: 225.0
+
+Square Root: 3.872983346207417
+
+Number: 16
+
+Squared: 256.0
+
+Square Root: 4.0
+
+Number: 17
+
+Squared: 289.0
+
+Square Root: 4.123105625617661
+
+Number: 18
+
+Squared: 324.0
+
+Square Root: 4.242640687119285
+
+Number: 19
+
+Squared: 361.0
+
+Square Root: 4.358898943540674
+
+Number: 20
+
+Squared: 400.0
+
+Square Root: 4.47213595499958
+
+Number: 21
+
+Squared: 441.0
+
+Square Root: 4.58257569495584
+
+Number: 22
+
+Squared: 484.0
+
+Square Root: 4.69041575982343
+
+Number: 23
+
+Squared: 529.0
+
+Square Root: 4.795831523312719
+
+Number: 24
+
+Squared: 576.0
+
+Square Root: 4.898979485566356
+
+Number: 25
+
+Squared: 625.0
+
+Square Root: 5.0
+
+Number: 26
+
+Squared: 676.0
+
+Square Root: 5.0990195135927845
+
+Number: 27
+
+Squared: 729.0
+
+Square Root: 5.196152422706632
+
+Number: 28
+
+Squared: 784.0
+
+Square Root: 5.291502622129181
+
+Number: 29
+
+Squared: 841.0
+
+Square Root: 5.385164807134504
+
+Number: 30
+
+Squared: 900.0
+
+Square Root: 5.477225575051661
+
+Number: 31
+
+Squared: 961.0
+
+Square Root: 5.5677643628300215
+
+Number: 32
+
+Squared: 1024.0
+
+Square Root: 5.656854249492381
+
+Number: 33
+
+Squared: 1089.0
+
+Square Root: 5.744562646538029
+
+Number: 34
+
+Squared: 1156.0
+
+Square Root: 5.830951894845301
+
+Number: 35
+
+Squared: 1225.0
+
+Square Root: 5.916079783099616
+
+Number: 36
+
+Squared: 1296.0
+
+Square Root: 6.0
+
+Number: 37
+
+Squared: 1369.0
+
+Square Root: 6.082762530298219
+
+Number: 38
+
+Squared: 1444.0
+
+Square Root: 6.164414002968976
+
+Number: 39
+
+Squared: 1521.0
+
+Square Root: 6.244997998398398
+
+Number: 40
+
+Squared: 1600.0
+
+Square Root: 6.324555320336759
+
+Number: 41
+
+Squared: 1681.0
+
+Square Root: 6.4031242374328485
+
+Number: 42
+
+Squared: 1764.0
+
+Square Root: 6.48074069840786
+
+Number: 43
+
+Squared: 1849.0
+
+Square Root: 6.557438524302
+
+Number: 44
+
+Squared: 1936.0
+
+Square Root: 6.6332495807108
+
+Number: 45
+
+Squared: 2025.0
+
+Square Root: 6.708203932499369
+
+Number: 46
+
+Squared: 2116.0
+
+Square Root: 6.782329983125268
+
+Number: 47
+
+Squared: 2209.0
+
+Square Root: 6.855654600401044
+
+Number: 48
+
+Squared: 2304.0
+
+Square Root: 6.928203230275509
+
+Number: 49
+
+Squared: 2401.0
+
+Square Root: 7.0
+
+Number: 50
+
+Squared: 2500.0
+
+Square Root: 7.0710678118654755
+
+ */
