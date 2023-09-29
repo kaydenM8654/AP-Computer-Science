60cf8504d88d33e2f4808d69728145e9c3a28d19
kayden <krazykayden77@gmail.com>
2023-09-28 19:00:07 -0500

    9/28

--- src/Q1/LP4_2.java
+++ src/Q1/LP4_2.java
@@ -5,31 +5,48 @@ public class LP4_2 {
         Scanner input = new Scanner(System.in);
 
 
-        System.out.print("Please enter number of copies: ");
+        System.out.print("Please enter weight in kg: ");
         int kilo = input.nextInt();
         System.out.println();
 
-        System.out.print("Please enter number of copies: ");
+        System.out.print("Please enter length: ");
         int len = input.nextInt();
         System.out.println();
 
-        System.out.print("Please enter number of copies: ");
+        System.out.print("Please enter wid: ");
         int wid = input.nextInt();
         System.out.println();
 
-        System.out.print("Please enter number of copies: ");
+        System.out.print("Please enter height: ");
         int high = input.nextInt();
         System.out.println();
         int ans = len * wid * high;
 
 
-        if (int weigh < 28 && ans < 100001) {
+
+
+        if (kilo < 28 && ans < 100001) {
             System.out.println();
+        }else if (kilo > 27 && ans < 100001) {
+            System.out.println("Too heavy");
+        }else if (kilo < 28 && ans > 100000) {
+            System.out.println("Too Large");
         }else {
-            System.out.println("Too heavy")
+            System.out.println("Too large and too heavy");
         }
 
 
 
     }
 }
+/*
+Please enter weight in kg: 32
+
+Please enter length: 10
+
+Please enter wid: 25
+
+Please enter height: 38
+
+Too heavy
+ */
---
+++ src/Q1/LP4_3.java
@@ -0,0 +1,32 @@
+package Q1;
+import java.util.*;
+public class LP4_3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print("Please enter number of eggs: ");
+        int eggs = input.nextInt();
+        System.out.println();
+        double num = 0;
+        int dozen = eggs / 12;
+
+        if (dozen > 0 && dozen < 4) {
+            num = 0.50;
+        }else if (dozen >= 4 && dozen < 6) {
+            num = 0.45;
+        }else if (dozen >= 6 && dozen < 11) {
+            num = 0.40;
+        }else if (dozen >= 11) {
+            num = 0.35;
+        }else {
+            System.out.println("Please enter valid number");
+        }
+        int extra = eggs - (dozen * 12);
+        double equation = extra * (1/12 * num);
+        double ans = dozen * num + equation;
+        System.out.println("The bill is to equal: $" + ans);
+        System.out.println(equation);
+
+    }
+}
