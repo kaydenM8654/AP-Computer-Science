7479b80183e1eec01e6e485a3f500f7723df62ed
madonna.k <krazykayden77@gmail.com>
2023-11-10 11:22:36 -0600

    11/10

---
+++ src/Q2/LP3_9Class/Cl3_9.java
@@ -0,0 +1,24 @@
+package Q2.LP3_9Class;
+
+public class Cl3_9 {
+    private int year;
+    private int month;
+    private int day;
+    private int year1;
+    private int month1;
+    private int day1;
+    private int alive;
+    private int slept;
+
+
+    public Cl3_9(int s) {
+        slept = s;
+        year = 0;
+        month = 0;
+        day = 0;
+        year1 = 0;
+        month1 = 0;
+        day1 = 0;
+        alive = 0;
+    }
+}
---
+++ src/Q2/LP3_9Class/LP3_9Cl.java
@@ -0,0 +1,4 @@
+package Q2.LP3_9Class;
+
+public class LP3_9Cl {
+}
