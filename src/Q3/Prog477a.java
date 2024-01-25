6eebcb99700d9261787d0dc887199ac27f9d3a5f
madonna.k <krazykayden77@gmail.com>
2024-01-25 11:15:39 -0600

    1/22

---
+++ src/Q3/Prog477a.java
@@ -0,0 +1,47 @@
+package Q3;
+import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Prog477a {
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("Langdat/prog477a.dat"));
+            int[][] array = new int[4][5];
+            int grade;
+            String A = "A";
+            String B = "B";
+            String C = "C";
+            String D = "D";
+            String F = "F";
+            int am = 0;
+            int bm = 0;
+            int cm = 0;
+            int dm = 0;
+            int fm = 0;
+            int af = 0;
+            int bf = 0;
+            int cf = 0;
+            int df = 0;
+            int ff = 0;
+            int at = 0;
+            int bt = 0;
+            int ct = 0;
+            int dt = 0;
+            int ft = 0;
+            int tt = 0;
+            int fmt = 0;
+            int mt = 0;
+
+            while (input.hasNext()){
+                if (grade )
+            }
+
+
+        } catch (IOException e) {
+            System.out.println("Can't find data file!");
+        }
+
+    }
+}
