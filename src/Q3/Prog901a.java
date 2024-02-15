a9027f2f25fdcd6ec3eb3987a32addf8398cead7
madonna.k <krazykayden77@gmail.com>
2024-02-15 11:20:00 -0600

    2/14

---
+++ src/Q3/Prog901a.java
@@ -0,0 +1,20 @@
+package Q3;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Prog901a {
+    public static void main(String[] args) {
+        FileReader n = new FileReader("Langdat/prog512h.dat");
+        Scanner in = new Scanner(n);
+        int i;
+        while(in.hasNext()){
+            i = in.nextInt();
+            System.out.println(i);
+        }
+        in.close();
+
+    }
+}
