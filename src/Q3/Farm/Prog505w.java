225536a21325b0c55535917ad17368ee3e27af3e
madonna.k <krazykayden77@gmail.com>
2024-02-21 11:34:34 -0600

    farm

--- src/Q3/Farm/Prog505w.java
+++ src/Q3/Farm/Prog505w.java
@@ -11,6 +11,9 @@ public class Prog505w {
         try {
             Scanner input = new Scanner(new File("Langdat/prog505w.dat"));
             List<Animal> animals = new ArrayList<Animal>();
+            int cweight = 0;
+            int hweight = 0;
+
 
             int hay = input.nextInt();
             double hayCost = input.nextDouble();
@@ -30,6 +33,7 @@ public class Prog505w {
                     animals.add(wow);
                     hay = hayEaten;
                     corn = cornEaten;
+                    cweight += weight;
                 }
             }
 
@@ -43,14 +47,18 @@ public class Prog505w {
                     int cornEaten = input.nextInt();
                     int rides = input.nextInt();
                     double rideCost = input.nextDouble();
-                    Horse wow = new Horse(name, weight, hayEaten, cornEaten, rides, rideCost);
+                    Horse wow = new Horse(name, weight, cornEaten, hayEaten, rides, rideCost);
                     animals.add(wow);
                     hay -= hayEaten;
                     corn -= cornEaten;
+                    hweight += weight;
                 }
             }
+            int cumlativeweight = cweight + hweight;
+            System.out.println("Cumlative weight of all animals: " + cumlativeweight);
 
-            //TODO: report income for the day, cumlative weight of all animals,if theres enough food to feed all, cow that makes most money
+            //TODO: report income for the day,if theres enough food to feed all, cow that makes most money
+
 
             double minHorseValue = Double.MAX_VALUE;
             int minHorseIndex = 0;
@@ -65,7 +73,21 @@ public class Prog505w {
                 }
             }
 
+            double maxCowValue = Double.MIN_VALUE;
+            int maxCowIndex = 0;
+            for (int lcv = 0; lcv < animals.size(); lcv++){
+                if (animals.get(lcv) instanceof Horse){
+                    Cow cow = (Cow)animals.get(lcv);
+                    double cvalue = cow.value(cornCost, hayCost);
+                    if(cvalue > maxCowValue){
+                        maxCowValue = cvalue;
+                        maxCowIndex = lcv;
+                    }
+                }
+            }
+
             System.out.printf("Horse %s makes the least money\n", animals.get(minHorseIndex).getName());
+            System.out.printf("Cow %s makes the most money\n", animals.get(maxCowIndex).getName());
 
         } catch (IOException e) {
             System.out.println("Can't find data file!");
